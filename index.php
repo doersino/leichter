c42376bc6ab7b87df5e2e5919c24527b6bf65f30
Noah Doersing <doersino@gmail.com>
2016-01-08 15:56:55 +0100

    Show message when less than two data points are available

--- index.php
+++ index.php
@@ -53,7 +53,6 @@ $weights = getWeights($start);
 			}
 			body {
 				font-size: 16px;
-				margin: 1em;
 				overflow: hidden;
 				font-family: Helvetica, sans-serif;
 			}
@@ -95,6 +94,16 @@ $weights = getWeights($start);
 			input[name="removeMostRecentWeight"]:hover {
 				text-decoration: none;
 			}
+			p {
+				position: absolute;
+				width: 100%;
+				top: 45%;
+				color: gray;
+				text-align: center;
+			}
+			div {
+				margin: 1em;
+			}
 		</style>
 	</head>
 	<body>
@@ -109,57 +118,65 @@ $weights = getWeights($start);
 			<input type="submit" name="addWeight" value="Add"><br>
 			<input type="submit" name="removeMostRecentWeight" value="remove most recent">
 		</form>
-		<canvas id="myChart"></canvas>
+		<?php if (sizeof($weights) == 0) { ?>
+			<p>No data available.</p>
+		<?php } else if (sizeof($weights) == 1) { ?>
+			<p>Found only one data point <?php if ($period != "all") echo "for this $period"; ?>:<br><?php echo $weights[0]["weight"]; ?>kg on <?php echo date("Y-m-d \a\\t H:i:s", $weights[0]["time"]); ?>.</p>
+		<?php } else { ?>
+			<div>
+				<canvas id="myChart"></canvas>
+			</div>
+			<script src="lib/jquery.min.js"></script>
+			<script src="lib/Chart.Core.min.js"></script>
+			<script src="lib/Chart.Scatter.min.js"></script>
+			<script>
+				// set chart dimensions
+				var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+				var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+				document.getElementById("myChart").width = w / 5;
+				document.getElementById("myChart").height = h / 5;
 
-		<script src="lib/jquery.min.js"></script>
-		<script src="lib/Chart.Core.min.js"></script>
-		<script src="lib/Chart.Scatter.min.js"></script>
+				// draw chart
+				Chart.defaults.global.responsive = true;
+				Chart.defaults.global.animation = false;
+				$(function () {
+					var data = [
+						{
+							label: 'smoothed weight',
+							strokeColor: '#A7A7D9',
+							pointColor: 'transparent',
+							pointStrokeColor: 'transparent',
+							data: <?php echo formatWeights(smoothWeights($weights)); ?>
+						},
+						{
+							label: 'weight',
+							strokeColor: '#A31515',
+							data: <?php echo formatWeights($weights); ?>
+						}];
+
+					var ctx = document.getElementById("myChart").getContext("2d");
+					var myChart = new Chart(ctx).Scatter(data, {
+						bezierCurve: false,
+						showTooltips: true,
+						scaleShowHorizontalLines: true,
+						scaleShowLabels: true,
+						scaleType: "date",
+						scaleLabel: "<%=value%> kg",
+						scaleOverride: true,
+						<?php $chartRange = getChartRange($weights); ?>
+						scaleSteps: <?php echo $chartRange["steps"]; ?>,
+						scaleStepWidth: <?php echo $chartRange["stepWidth"]; ?>,
+						scaleStartValue: <?php echo $chartRange["startValue"]; ?>
+					});
+				});
+			</script>
+		<?php } ?>
 		<script>
 			// update time period for chart
 			var sel = document.getElementById("period");
 			period.onchange = function() {
 				window.location.href = "index.php?period=" + this.value;
 			}
-
-			// set chart dimensions
-			var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-			var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-			document.getElementById("myChart").width = w / 5;
-			document.getElementById("myChart").height = h / 5;
-
-			// draw chart
-			Chart.defaults.global.responsive = true;
-			Chart.defaults.global.animation = false;
-			$(function () {
-				var data = [
-					{
-						label: 'smoothed weight',
-						strokeColor: '#A7A7D9',
-						pointColor: 'transparent',
-						pointStrokeColor: 'transparent',
-						data: <?php echo formatWeights(smoothWeights($weights)); ?>
-					},
-					{
-						label: 'weight',
-						strokeColor: '#A31515',
-						data: <?php echo formatWeights($weights); ?>
-					}];
-
-				var ctx = document.getElementById("myChart").getContext("2d");
-				var myChart = new Chart(ctx).Scatter(data, {
-					bezierCurve: false,
-					showTooltips: true,
-					scaleShowHorizontalLines: true,
-					scaleShowLabels: true,
-					scaleType: "date",
-					scaleLabel: "<%=value%> kg",
-					scaleOverride: true,
-					<?php $chartRange = getChartRange($weights); ?>
-					scaleSteps: <?php echo $chartRange["steps"]; ?>,
-					scaleStepWidth: <?php echo $chartRange["stepWidth"]; ?>,
-					scaleStartValue: <?php echo $chartRange["startValue"]; ?>
-				});
-			});
 		</script>
 	</body>
 </html>
